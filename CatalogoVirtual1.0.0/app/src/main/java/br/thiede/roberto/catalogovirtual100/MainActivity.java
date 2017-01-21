2273fb3b100439efe79c150420ea5499d66bf3c9
rthiede91 <rthiede91@gmail.com>
2017-01-21 11:11:25 -0200

    Inicio do Desenvolvimento

--- CatalogoVirtual1.0.0/app/src/main/java/br/thiede/roberto/catalogovirtual100/MainActivity.java
+++ CatalogoVirtual1.0.0/app/src/main/java/br/thiede/roberto/catalogovirtual100/MainActivity.java
@@ -1,12 +1,14 @@
 package br.thiede.roberto.catalogovirtual100;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
