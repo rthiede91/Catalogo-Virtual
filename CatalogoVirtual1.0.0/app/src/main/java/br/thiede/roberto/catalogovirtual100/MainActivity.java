0fb4be081bc47c1faf68cbab6a86123ebb0d748f
rthiede91 <rthiede91@gmail.com>
2017-01-21 10:40:42 -0200

    Criando o Projeto 1.0.0

---
+++ CatalogoVirtual1.0.0/app/src/main/java/br/thiede/roberto/catalogovirtual100/MainActivity.java
@@ -0,0 +1,13 @@
+package br.thiede.roberto.catalogovirtual100;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
