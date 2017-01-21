58867e836c036dda667cead35fa1e9dff305b978
Roberto Thiede <rthiede91@gmail.com>
2017-01-21 21:20:14 -0200

    Variaveis staticas App 40%
    banco de dados 80%
        . definir nome das tabelas e colunas
        . fazer o update com banco remoto.
    
     download images 60%
         . fazer o download apos a atualização do banco de dados
    
    design 0%
        . Criar tela de apresentação (Empresa)
        . Criar tela de categorias
        . Criar tela de produtos
        . Criar anuncio

---
+++ CatalogoVirtual1.0.0/app/src/main/java/br/thiede/roberto/catalogovirtual100/configs/standardVars.java
@@ -0,0 +1,72 @@
+package br.thiede.roberto.catalogovirtual100.configs;
+
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * Created by thiedes on 21/01/17.
+ */
+
+public class standardVars
+{
+    public static final  int appVersion = 100;
+
+    public static final  String imagesPath = Environment.getExternalStorageDirectory()+"/CatalogoVirtual/";
+    public static final  String imagesType = "image/jpeg";
+    public static final  int imagesQuality = 50;
+
+    public static final  String urlPhpUpdateDB = "http://octaltech.com.br/catalogovirtual/php/update.php";
+    public static final  String urlPhpUpgradeDB = "http://octaltech.com.br/catalogovirtual/php/upgrade.php";
+    public static final  String urlImages = "http://octaltech.com.br/catalogovirtual/images/";
+
+    public static final  String spacer1 = "#@#";
+    public static final  String spacer2 = "!*!";
+    public static final  String spacer3 = "$%$";
+
+    public static final  String databaseName = "catalogovirtual";
+    public static final  int databaseVersion = 100;
+
+    public static final  String[][] databaseTables = {
+            {
+                    "business(",
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT,",
+                    "date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,",
+                    "name varchar(100) NOT NULL,",
+                    "price int(10) NOT NULL,",
+                    "description text NOT NULL,",
+                    "images text NOT NULL,",
+                    "category varchar(150) NOT NULL,",
+                    "sub_category varchar(150) NOT NULL,",
+                    "ref varchar(50) DEFAULT NULL",
+                    ")"
+            },
+            {
+                    "category(",
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT,",
+                    "date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,",
+                    "name varchar(100) NOT NULL,",
+                    "price int(10) NOT NULL,",
+                    "description text NOT NULL,",
+                    "images text NOT NULL,",
+                    "category varchar(150) NOT NULL,",
+                    "sub_category varchar(150) NOT NULL,",
+                    "ref varchar(50) DEFAULT NULL",
+                    ")"
+            },
+            {
+                    "product(",
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT,",
+                    "date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,",
+                    "name varchar(100) NOT NULL,",
+                    "price int(10) NOT NULL,",
+                    "description text NOT NULL,",
+                    "images text NOT NULL,",
+                    "category varchar(150) NOT NULL,",
+                    "sub_category varchar(150) NOT NULL,",
+                    "ref varchar(50) DEFAULT NULL",
+                    ")"
+            },
+    };
+
+}
